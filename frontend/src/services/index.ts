4e6ba7da21181da683106914fb114889c07e7520
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-04-10 23:40:06 +0600

    add: service index

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,4 @@
+export { accountService } from "./account.service";
+export { transactionService } from "./transaction.service";
+export { requestJson, API_BASE } from "./http";
+export { socketService } from "./socket";
