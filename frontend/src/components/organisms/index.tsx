96b47f55232fdf2e2a6a181d37ed1db1604a0a59
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-04-10 22:21:35 +0600

    add: index for organism

---
+++ frontend/src/components/organisms/index.tsx
@@ -0,0 +1,6 @@
+export { Header } from "./Header";
+export { Navbar } from "./Navbar";
+export { Layout } from "./Layout";
+export { AccountCard } from "./AccountCard";
+export { NotificationList } from "./NotificationList";
+export { RealtimeIndicator } from "./RealtimeIndicator";
