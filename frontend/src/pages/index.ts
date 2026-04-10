ca1b644845a81d56b09ca022dc61c3370f26812b
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-04-10 22:31:36 +0600

    add: index for pages

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { DashboardPage } from "./Dashboard";
+export { AccountsPage } from "./Accounts";
+export { TransactionsPage } from "./Transactions";
+export { NotFoundPage } from "./NotFound";
